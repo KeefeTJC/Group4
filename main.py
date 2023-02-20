2ab85405ecfac0bacb3b579256e5a7431b3c9661
KeefeTJC <117626526+KeefeTJC@users.noreply.github.com>
2023-02-20 16:19:08 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import scrapy
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['http://brickset.com/sets/year-2019']
+
+    def parse(self, response):
+        xpath_selector = '//img'
+        for x in response.xpath(xpath_selector):
+            newsel = '@src'
+            yield {'Image Link': x.xpath(newsel).extract_first(), }
+
+# To Recurse next page
+        page_selector = '.next a ::attr(href)'
+        next_page = response.css(page_selector).extract_first
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
