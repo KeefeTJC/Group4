5a32fc7ffd72ea6bde482d49e03dc2472df9c26e
KeefeTJC <117626526+KeefeTJC@users.noreply.github.com>
2023-02-27 11:05:02 +0800

    Update TestMyProgram.py

--- TestMyProgram.py
+++ TestMyProgram.py
@@ -1,7 +1,9 @@
 import unittest
 import requests
 
-url = "http://172.18.58.80/varsitys"
+
+url = "http://172.18.58.80/index.html"
+
 
 # Each test class must be a subclass of unittest.TestCase
 # The class should be named as TestXXXX to indicate to the program that it is a test program
@@ -15,11 +17,21 @@ class TestMyProgram(unittest.TestCase):
             resp = requests.get(url)
             if int(resp.status_code) == 200:
                 print("[TestUrl] URL OK")
-                print("Pass")
-
             else:
                 print("[TestUrl] Requested URL not found")
-                print("Fail")
-
         except Exception as e:
             print("[TestUrl] Error: ", {e})
+
+    def test_TestCase_2(self):
+        print("[TestCase_2] Test case 2")
+
+import unittest
+from scrapytest import NewSpider
+
+class TestNewSpider(unittest.TestCase):
+    def test_start_urls(self):
+        self.assertEqual(NewSpider().start_urls[0], 'http://172.18.58.80/varsity')
+
+# Must invoke the unittest.main() methods
+if __name__ == '_main_':
+    unittest.main()
